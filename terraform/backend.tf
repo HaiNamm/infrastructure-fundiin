c78c7a55220a78e07bb72cee4960831fd35b329e
HaiNamm <dhtn8xx@gmail.com>
2024-12-12 17:50:43 +0700

    update file backeend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
+    bucket = "hhainam23-app"
+    region = "ap-southeast-1"
     key = "eks/terraform.tfstate"
   }
 }
